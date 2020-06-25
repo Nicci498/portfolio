34ebfab57d47cbd3f3c4632da7f9fce8e0ae263d
Nicole white <you@example.com>
2020-06-24 20:17:53 -1000

    background image and animation in, basic component structure built

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './App.css';
+import Title from './components/Title.js'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Title />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Title.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Title(){
+    return(
+        <>
+        <h1 className = 'name'>Nicole White</h1>
+        <p className = 'subtitle'>Full Stack Web Developer</p>
+        </>
+    )
+}
+export default Title;
