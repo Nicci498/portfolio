a963fb6cfc5f7a21965ea8910aebf06e0058478a
Nicole White <nicci498@gmail.com>
2020-09-12 19:53:44 -1000

    about stuff

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -4,8 +4,9 @@ import React from 'react';
 
 function AboutSection({setShowAbout}){
     return(
-        <div className='about-section'>
-            <h3>About section baby</h3>
+        <div className = 'about-section'>
+            <h3 className = 'about-title'>Nicole White</h3>
+            <p className = 'about'>I am a <span className = 'role'>Full Stack Web Developer</span> living in Hawaii with a passion for coding, traveling, reading, and great food. I finished the Full Stack Web Development Program at Lambda School in July of 2020. I also currently work for Lambda School as a TL for a fantastic team building a mobile first app to display earthquake data. I am looking forward to new and exciting adventures and learning and growth opportunities.</p>
             <h6 onClick={() => setShowAbout(false)}>Exit About</h6>
         </div>
     )
